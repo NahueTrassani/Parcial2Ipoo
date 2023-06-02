7a2922fc1143ab381db61748cf8dd26ae9858470
NahueTrassani <71827787+NahueTrassani@users.noreply.github.com>
2023-06-02 18:02:41 -0300

    add files
    123456

---
+++ SegundoParcial_TrassaniMauroNahuel/VehiculoImportado.php
@@ -0,0 +1,46 @@
+
+<?php
+
+class VehiculoImportado extends Vehiculo {
+    private $paisOrigen;
+    private $impuestosImportacion;
+
+    public function __construct($codigo, $costo, $anioFabricacion, $descripcion, $porcentajeIncrementoAnual, $activo, $paisOrigen, $impuestosImportacion) {
+        parent::__construct($codigo, $costo, $anioFabricacion, $descripcion, $porcentajeIncrementoAnual, $activo);
+        $this->paisOrigen = $paisOrigen;
+        $this->impuestosImportacion = $impuestosImportacion;
+    }
+
+    public function getPaisOrigen() {
+        return $this->paisOrigen;
+    }
+
+    public function setPaisOrigen($paisOrigen) {
+        $this->paisOrigen = $paisOrigen;
+    }
+
+    public function getImpuestosImportacion() {
+        return $this->impuestosImportacion;
+    }
+
+    public function setImpuestosImportacion($impuestosImportacion) {
+        $this->impuestosImportacion = $impuestosImportacion;
+    }
+
+    public function __toString() {
+        return "Vehículo Importado\n" .
+        "Código: " . $this->getCodigo() . "\n" .
+        "Costo: " . $this->getCosto() . "\n" .
+        "Año de fabricación: " . $this->getAnioFabricacion() . "\n" .
+        "Descripción: " . $this->getDescripción() . "\n" .
+        "Porcentaje de incremento anual: ". $this->getPorcentajeAnual() . "\n" .
+        "País de Origen: " . $this->paisOrigen . "\n" .
+        "Impuestos de Importación: $" . $this->impuestosImportacion . "\n";
+    }
+
+ 
+    public function darPrecioVentaImportado($valorCalculado) {
+        return $valorCalculado + $this->impuestosImportacion;
+    }
+}
+?>
---
+++ SegundoParcial_TrassaniMauroNahuel/Venta.php
@@ -0,0 +1,114 @@
+
+<?php
+
+class Venta {
+    private $numero;
+    private $fecha;
+    private $cliente;
+    private $vehiculos = array();
+    private $precioFinal;
+
+    public function __construct($numero, $fecha, $cliente, $vehiculos) {
+        $this->numero = $numero;
+        $this->fecha = $fecha;
+        $this->cliente = $cliente;
+        $this->vehiculos = [];
+        $this->precioFinal = 0; // Inicializar el precio final en 0 al crear una venta
+    }
+
+    // Métodos set y get de la clase
+    public function setNumero($numero) {
+        $this->numero = $numero;
+    }
+
+    public function getNumero() {
+        return $this->numero;
+    }
+
+    public function setFecha($fecha) {
+        $this->fecha = $fecha;
+    }
+
+    public function getFecha() {
+        return $this->fecha;
+    }
+
+    public function setCliente($cliente) {
+        $this->cliente = $cliente;
+    }
+
+    public function getCliente() {
+        return $this->cliente;
+    }
+
+    public function incorporarVehiculo($vehiculo) {
+        $precio = $vehiculo->darPrecioVenta();
+      
+        $colVehiculos = $this->getVehiculos();
+        array_push($colVehiculos, $vehiculo);
+        $this->setVehiculos($colVehiculos);
+
+        $precioTotal = $this->getPrecioFinal() + $precio;
+        $this->setPrecioFinal($precioTotal);
+    }
+
+    public function setVehiculos($vehiculos) {
+        $this->vehiculos = $vehiculos;
+    }
+
+    public function getVehiculos() {
+        return $this->vehiculos;
+    }
+
+    public function setPrecioFinal($precioFinal) {
+        $this->precioFinal = $precioFinal;
+    }
+
+    public function getPrecioFinal() {
+        return $this->precioFinal;
+    }
+
+    // Método toString
+    public function __toString() {
+       
+        $vehiculos_str = "";
+        foreach ($this->vehiculos as $vehiculo) {
+            $vehiculos_str .= "\n". $vehiculo . "\n";
+        }
+       
+        return  "\n". "Número de venta: " . $this->numero . "\n" .
+                "Fecha de venta: " . $this->fecha . "\n" .
+                "Cliente: " . $this->cliente . "\n" .
+              //"Aca mostraria los vehiculos de la venta, pero estan ocultos";
+               //"Vehículos de la venta:\n" . $vehiculos_str ."\n".
+               "Precio Final de la venta: " . $this->precioFinal;
+    }
+
+    public function retornarTotalVentaNacional() {
+        $totalVentaNacional = 0;
+        
+        foreach ($this->getVehiculos() as $vehiculo) {
+            if ($vehiculo instanceof VehiculoNacional) {
+               
+                $totalVentaNacional += $vehiculo->darPrecioVenta();
+                
+            }
+        }
+
+        return $totalVentaNacional;
+    }
+
+    public function retornarVehiculosImportados() {
+        $vehiculosImportados = [];
+
+        foreach ($this->vehiculos as $vehiculo) {
+            if ($vehiculo instanceof VehiculoImportado) {
+                $vehiculosImportados[] = $vehiculo;
+            }
+        }
+
+        return $vehiculosImportados;
+    }
+}
+
+?>
